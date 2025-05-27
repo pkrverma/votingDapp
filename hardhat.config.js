ffbe4b217f54b7bcf18e31174de08c386a66d5f4
Pulkit verma <pulkit18012003@gmail.com>
2025-05-27 16:43:09 +0530

    secret key is added with upadting gitignore file. hardhat.config file is also updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,44 @@
-require("@nomicfoundation/hardhat-toolbox");
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-ethers');
+require("@nomiclabs/hardhat-waffle");
+
+const { PrivateKey } = require('./secret.json');
+
 module.exports = {
-  solidity: "0.8.28",
+   defaultNetwork: 'testnet',
+
+   networks: {
+      hardhat: {
+      },
+      testnet: {
+         url: 'https://rpc.test.btcs.network',
+         accounts: [PrivateKey],
+         chainId: 1115,  // Ensure this matches the Core testnet chain ID
+      }
+   },
+   solidity: {
+      compilers: [
+        {
+           version: '0.8.21',  // Update to at least 0.8.20 to support the 'paris' EVM
+           settings: {
+              evmVersion: 'paris',  // Specify 'paris' EVM version
+              optimizer: {
+                 enabled: true,
+                 runs: 200,
+              },
+           },
+        },
+      ],
+   },
+   paths: {
+      sources: './contracts',
+      cache: './cache',
+      artifacts: './artifacts',
+   },
+   mocha: {
+      timeout: 20000,  // You can adjust the timeout as needed
+   },
 };
